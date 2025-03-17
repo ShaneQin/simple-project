c9f9bac1f18cc23040a9b88f6aaa14fac1af0eeb
ShaneQin <664633146@qq.com>
2025-03-17 22:55:01 +0800

    iframe 沙盒

---
+++ micro-framework-iframe-sandbox/public/micro/micro1.js
@@ -0,0 +1,4 @@
+let root;
+root = document.createElement('h1');
+root.textContent = '微应用1';
+document.body.appendChild(root);
---
+++ micro-framework-iframe-sandbox/public/micro/micro2.js
@@ -0,0 +1,4 @@
+let root;
+root = document.createElement('h1');
+root.textContent = '微应用2';
+document.body.appendChild(root);
