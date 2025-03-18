a0c41fae1df7a6f05725ba5b5c0beeee99b8e52e
ShaneQin <664633146@qq.com>
2025-03-18 09:38:15 +0800

    快照隔离

---
+++ micro-framework-snapshot-sandbox/public/micro/micro2.js
@@ -0,0 +1,8 @@
+let root = document.createElement('h1');
+root.textContent = '微应用2';
+root.id = 'micro2-dom';
+root.onclick = () => {
+  console.log('微应用2 的 window.a: ', window.a);
+}
+document.body.appendChild(root);
+window.a = 2;
